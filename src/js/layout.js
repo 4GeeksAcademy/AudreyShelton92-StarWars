5185029adbeca793ed69f69e52e18f984fb5f63d
AudreyShelton92 <83794598+AudreyShelton92@users.noreply.github.com>
2024-10-22 01:03:12 +0000

    first save, stopped on APIs

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,6 +5,8 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+import CharacterCard from "./component/characterCards";
+import characterDescription from "./views/characterDescription";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
