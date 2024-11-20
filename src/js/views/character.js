8cb546b6676d4090b581a11484d99ad42f5e8f42
AudreyShelton92 <83794598+AudreyShelton92@users.noreply.github.com>
2024-11-20 17:06:55 +0000

    completed

--- src/js/views/character.js
+++ src/js/views/character.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 
-export default function CharacterDescription() { 
+export default function CharacterDescription() {
   const { id } = useParams();
   const [character, setCharacter] = useState({});
 
@@ -10,20 +10,20 @@ export default function CharacterDescription() {
       let response = await fetch(`https://www.swapi.tech/api/people/${id}`);
       let data = await response.json();
       setCharacter(data.result.properties);
-    } 
+    }
     getCharacter();
-  } , [id] ) ;
+  }, [id]);
 
   return (
     <>
-       <div>
-            <h1>{character.name}</h1>
-            <p>Gender: {character.gender}</p>
-            <p>Hair Color: {character.hair_color}</p>
-            <p>Height: {character.height}</p>
-            <p>Birth Year: {character.birth_year}</p>
-        
-            </div>
+      <div>
+        <h1>{character.name}</h1>
+        <p>Gender: {character.gender}</p>
+        <p>Hair Color: {character.hair_color}</p>
+        <p>Height: {character.height}</p>
+        <p>Birth Year: {character.birth_year}</p>
+
+      </div>
     </>
   );
 }
